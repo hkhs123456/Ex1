9f0c35179bd75596c240c13bbedf1622cf376d0c
hkhs123456 <131226843+hkhs123456@users.noreply.github.com>
2023-04-20 11:30:59 +0800

    刪掉晃動

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onGesture(Gesture.Shake, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-})
 basic.showIcon(IconNames.Heart)
 basic.showString("Hello!")
 music.playMelody("- - - - - - - - ", 120)
